95403d6d90f7d0d3593e9a3762424962151e56c5
Jesse Ian Cary <jesseiancary@gmail.com>
2020-02-05 09:50:15 -0700

    Updated HL7 UpdateView

--- client/src/views/hl7/update.js
+++ client/src/views/hl7/update.js
@@ -5,63 +5,37 @@ class UpdateView extends Component {
   render() {
     const t = this.props.this;
     return (
-      <div className="update person">
+      <div className="update hl7-document">
         <div className="container">
 
           <div className="row">
             <div className="col-md-8 m-auto">
               <br />
-              <Link to="/people" className="btn btn-outline-warning">
-                Show All People
+              <Link to="/hl7" className="btn btn-outline-warning">
+                Show All HL7 Documents
               </Link>
               <br />
               <br />
             </div>
             <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Edit Person</h1>
-              <p className="lead text-center">
-                Update Person's Info
-              </p>
+              <h1 className="display-4 text-center">Edit HL7</h1>
             </div>
           </div>
   
           <div className="col-md-8 m-auto">
             <form noValidate onSubmit={t.onSubmit}>
 
-              <div className='form-group'>
-                <input
-                  type='text'
-                  name='first_name'
-                  placeholder='First Name'
-                  className='form-control'
-                  value={t.state.first_name}
-                  onChange={t.onChange}
-                />
-              </div>
+                <div className='form-group'>
+                  <textarea
+                    name='hl7_data'
+                    placeholder='HL7 Data'
+                    className='form-control'
+                    value={t.state.hl7_data}
+                    onChange={t.onChange}
+                  />
+                </div>
   
-              <div className='form-group'>
-                <input
-                  type='text'
-                  name='last_name'
-                  placeholder='Last Name'
-                  className='form-control'
-                  value={t.state.last_name}
-                  onChange={t.onChange}
-                />
-              </div>
-  
-              <div className='form-group'>
-                <input
-                  type='date'
-                  name='dob'
-                  placeholder='Date of Birth'
-                  className='form-control'
-                  value={t.state.dob}
-                  onChange={t.onChange}
-                />
-              </div>
-  
-              <button type="submit" className="btn btn-outline-info btn-lg btn-block">Update Person</button>
+              <button type="submit" className="btn btn-outline-info btn-lg btn-block">Update HL7</button>
               <br />
 
             </form>
