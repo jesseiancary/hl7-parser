f334795d2a04abf386d53653289668cb93bdb221
Jesse Ian Cary <jesseiancary@gmail.com>
2020-02-06 09:11:42 -0700

    Updated Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,66 +11,63 @@ class Landing extends Component {
   render() {
   
     const loggedOut = (
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link to="/users/login" className="nav-link">
-            Login
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/users/new" className="nav-link">
-            Register
-          </Link>
-        </li>
-      </ul>
+      <div className="dropdown-menu dropdown-menu-right">
+        <Link to="/login" className="dropdown-item">
+          <i className="fas fa-sign-in-alt fa-fw"></i> Login
+        </Link>
+        <Link to="/register" className="dropdown-item">
+          <i className="fas fa-user-plus fa-fw"></i> Register
+        </Link>
+      </div>
     );
 
     const loggedIn = (
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link to="/users" className="nav-link">
-            Users
-          </Link>
-        </li>
-        <li className="nav-item">
-          <a href="" onClick={this.logOut.bind(this)} className="nav-link">
-            Logout
-          </a>
-        </li>
-      </ul>
+      <div className="dropdown-menu dropdown-menu-right">
+        <Link to="/profile" className="dropdown-item">
+          <i className="fas fa-user-edit fa-fw"></i> Profile
+        </Link>
+        <a href="#" onClick={this.logOut.bind(this)} className="dropdown-item">
+          <i className="fas fa-sign-out-alt fa-fw"></i> Logout
+        </a>
+      </div>
     );
 
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarsExample10"
-          aria-controls="navbarsExample10"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-
-        <div
-          className="collapse navbar-collapse justify-content-md-center"
-          id="navbarsExample10"
-        >
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/hl7/new" className="nav-link">
-                HL7 Parser
-              </Link>
-            </li>
-          </ul>
-          {localStorage.usertoken ? loggedIn : loggedOut}
+      <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+        <div className="container">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/hl7/new" className="nav-link">
+                  HL7
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav">
+              <li className="nav-item dropdown">
+                <a className="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                  <i className="fas fa-user"></i>
+                </a>
+                {localStorage.usertoken ? loggedIn : loggedOut}
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
